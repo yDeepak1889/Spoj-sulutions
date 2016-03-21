1e1f9c9093fa6a00b8286a7c249c9269b72f2fc4
Deepak kumar yadav <ydeepak1889@gmail.com>
2016-03-21 17:09:37 +0530

    Create TEST.c

---
+++ TEST.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void)
+{
+	int num=0;
+		while(1)
+		{
+			num=0;
+			scanf("%d",&num);
+				
+			if(num==42)
+			{
+			
+				break;
+			}
+			printf("%d\n",num);
+		}
+	return 0;
+} 
